c09466df84933781661a5496309cef97ea26c1a8
DeViLaXs <oamsa20@gmail.com>
2026-02-08 22:56:38 +0300

    edit login and register styles

--- src/app/(auth)/_components/auth-navbar.tsx
+++ src/app/(auth)/_components/auth-navbar.tsx
@@ -10,13 +10,29 @@ import { Sun, Moon } from 'lucide-react'
 import { Button } from '../../../components/ui/button'
 import { useTheme } from 'next-themes'
 import { AnimatedThemeToggler } from '@/components/ui/animated-theme-toggler'
+import Image from 'next/image'
 
 export default function AuthNavBar() {
   const { setTheme } = useTheme()
   return (
-    <nav className="bg-background/90 sticky top-0 z-50 flex items-center justify-between border-b px-6 py-4 shadow-sm max-sm:px-4 max-sm:py-3 md:px-12">
+    <nav className="bg-card sticky top-0 z-50 flex items-center justify-between border-b px-6 py-4 shadow-sm max-sm:px-4 max-sm:py-3 md:px-12">
       <div className="flex w-full items-center justify-between">
-        <div className="text-primary text-2xl font-bold">GoWork</div>
+        <div className="h-10 flex items-center justify-center">
+          <Image
+            src="/masarak-dark.png"
+            className="hidden dark:block"
+            alt="Logo"
+            width={130}
+            height={130}
+          />
+          <Image
+            src="/masarak-light.png"
+            className="block dark:hidden"
+            alt="Logo"
+            width={130}
+            height={130}
+          />
+        </div>
         <AnimatedThemeToggler />
       </div>
     </nav>
